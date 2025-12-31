9a1332a2e432cff04c98672340852775014744af
Criss-Ivana <cristian.m.ivana@gmail.com>
2025-12-31 18:08:17 +0200

    vlog call fix

--- kernel/source/panic.c
+++ kernel/source/panic.c
@@ -1,15 +1,15 @@
 #include "panic.h"
-#include "log.h"
+
 #include "arch/lcpu.h"
+#include "log.h"
 
 void panic(const char *format, ...)
 {
     arch_lcpu_int_mask();
 
     log(LOG_FATAL, "PANIC");
-
     va_list vargs;
-    va_start(vargs, format);
+    va_start(vargs);
     vlog(LOG_FATAL, NULL, format, vargs);
     va_end(vargs);
 
