81d6e6e2b91beb9f01b72fb101f9d551d85d0273
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2026-01-09 14:20:07 +0200

    add sleep syscall

--- kernel/source/sys/proc.c
+++ kernel/source/sys/proc.c
@@ -1,8 +1,12 @@
+#include "proc/smp.h"
+#include "proc/thread.h"
 #include "sys/syscall.h"
 
 #include "arch/misc.h"
+#include "arch/timer.h"
 #include "log.h"
 #include "proc/sched.h"
+#include <stdint.h>
 
 sys_ret_t syscall_exit(int code)
 {
@@ -20,5 +24,13 @@ sys_ret_t syscall_tcb_set(void *ptr)
 {
     arch_syscall_tcb_set(ptr);
 
+    return (sys_ret_t) {0, EOK};
+}
+
+// sleep in microseconds
+sys_ret_t syscall_sleep(unsigned us)
+{
+    sys_curr_thread()->sleep_until = arch_timer_get_uptime_ns() + (uint64_t) us * 1000;
+    sched_yield(THREAD_STATE_SLEEPING);
     return (sys_ret_t) {0, EOK};
 }
