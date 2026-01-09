32af01968962c18fac81d5360e612f601305f4b9
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2026-01-09 13:24:46 +0200

    fix hpet
    move defines and table struct to new file

--- kernel/include/arch/x86_64/devices/hpet.h
+++ kernel/include/arch/x86_64/devices/hpet.h
@@ -1,61 +1,6 @@
 #pragma once
 
-#define HPET_GENERAL_CAPABILITIES   0x00
-#define HPET_GENERAL_CONFIG         0x10
-#define HPET_GENERAL_INT_STATUS     0x20
-#define HPET_MAIN_COUNTER_VALUE     0xF0 
-
-// (N = 0-31)
-#define HPET_TIMER_CONFIG(n)        (0x100 + (n) * 0x20)
-#define HPET_TIMER_COMPARATOR(n)    (0x108 + (n) * 0x20)
-
-// General config
-#define HPET_CONFIG_ENABLE          (1 << 0) // HPET on
-#define HPET_CONFIG_LEGACY_RT       (1 << 1)
-
-// Per timer
-#define HPET_TIMER_INT_ENABLE       (1 << 2) // Timer for interrupts
-#define HPET_TIMER_TYPE_PERIODIC    (1 << 3) // Periodic || one-shot
-#define HPET_TIMER_CAP_PERIODIC     (1 << 4)
-#define HPET_TIMER_CAP_64BIT        (1 << 5)
-#define HPET_TIMER_SET_ACCUMULATOR  (1 << 6)
-#define HPET_TIMER_FORCE_32BIT      (1 << 8)
-
-typedef struct
-{
-    uint8_t  address_space_id;
-    uint8_t  register_bit_width;
-    uint8_t  register_bit_offset;
-    uint8_t  reserved;
-    uint64_t address;
-}
-__attribute__((packed))
-hpet_address_t;
-
-typedef struct
-{
-    char            signature[4];
-    uint32_t        length;
-    uint8_t         revision;
-    uint8_t         checksum;
-    char            oem_id[6];
-    char            oem_table_id[8];
-    uint32_t        oem_revision;
-    uint32_t        creator_id;
-    uint32_t        creator_revision;
-    uint8_t         hardware_rev_id;
-    uint8_t         comparator_count : 5;
-    uint8_t         counter_size : 1;
-    uint8_t         reserved : 1;
-    uint8_t         legacy_replacement : 1;
-    uint16_t        pci_vendor_id;
-    hpet_address_t  address;
-    uint8_t         hpet_number;
-    uint16_t        minimum_tick;
-    uint8_t         page_protection;
-}
-__attribute__((packed))
-hpet_table_t;
+#include <stdint.h>
 
 bool hpet_init();
 uint64_t hpet_get_frequency();
--- kernel/source/arch/x86_64/devices/hpet.c
+++ kernel/source/arch/x86_64/devices/hpet.c
@@ -1,10 +1,34 @@
+#include "arch/x86_64/devices/hpet.h"
 #include "arch/timer.h"
+//
 #include "hhdm.h"
-#include "include/arch/x86_64/devices/hpet.h"
-#include "include/dev/acpi/acpi.h"
+#include "dev/acpi/acpi.h"
+#include "dev/acpi/tables/hpet.h"
 #include "log.h"
 #include "mm/mm.h"
 #include "uapi/errno.h"
+#include <stdint.h>
+
+#define HPET_GENERAL_CAPABILITIES   0x00
+#define HPET_GENERAL_CONFIG         0x10
+#define HPET_GENERAL_INT_STATUS     0x20
+#define HPET_MAIN_COUNTER_VALUE     0xF0
+
+// (N = 0-31)
+#define HPET_TIMER_CONFIG(n)        (0x100 + (n) * 0x20)
+#define HPET_TIMER_COMPARATOR(n)    (0x108 + (n) * 0x20)
+
+// General config
+#define HPET_CONFIG_ENABLE          (1 << 0) // HPET on
+#define HPET_CONFIG_LEGACY_RT       (1 << 1)
+
+// Per timer
+#define HPET_TIMER_INT_ENABLE       (1 << 2) // Timer for interrupts
+#define HPET_TIMER_TYPE_PERIODIC    (1 << 3) // Periodic || one-shot
+#define HPET_TIMER_CAP_PERIODIC     (1 << 4)
+#define HPET_TIMER_CAP_64BIT        (1 << 5)
+#define HPET_TIMER_SET_ACCUMULATOR  (1 << 6)
+#define HPET_TIMER_FORCE_32BIT      (1 << 8)
 
 static volatile void *hpet_base = NULL;
 static uint64_t hpet_period_fs = 0;
@@ -21,14 +45,14 @@ static inline void hpet_write_reg(uint64_t offset, uint64_t value)
 
 bool hpet_init()
 {
-    hpet_table_t *hpet_table = (hpet_table_t *)acpi_lookup("HPET");
+    acpi_hpet_table_t *hpet_table = (acpi_hpet_table_t *)acpi_lookup("HPET");
     if(!hpet_table)
         return false;
 
     hpet_base = (volatile void *)(uintptr_t)(hpet_table->address.address + HHDM);
     uint64_t capabilities = hpet_read_reg(HPET_GENERAL_CAPABILITIES);
     hpet_period_fs = capabilities >> 32;
-    
+
     // Disable, set main counter to 0, enable
     uint64_t config = hpet_read_reg(HPET_GENERAL_CONFIG);
     config &= ~HPET_CONFIG_ENABLE;
@@ -45,7 +69,7 @@ uint64_t hpet_get_frequency()
 {
     if (hpet_period_fs == 0)
         return 0;
-    
+
     return 1000000000000000ULL / hpet_period_fs;
 }
 
@@ -53,7 +77,7 @@ uint64_t hpet_read_counter()
 {
     if (!hpet_base)
         return 0;
-    
+
     return hpet_read_reg(HPET_MAIN_COUNTER_VALUE);
 }
 
@@ -63,7 +87,7 @@ void hpet_sleep_ns(uint64_t nanoseconds)
         return;
 
     uint64_t ticks = (nanoseconds * 1000000ULL) / hpet_period_fs;
-    
+
     uint64_t start = hpet_read_counter();
     uint64_t end = start + ticks;
 
